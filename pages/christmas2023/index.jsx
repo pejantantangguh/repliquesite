09c70a83f9586c78d4426fe92bb9afdda1f341cd
Herman Ng <suherman.ng@gmail.com>
2023-12-23 18:40:46 +1000

    Add image to christmas page

--- pages/christmas2023/index.jsx
+++ pages/christmas2023/index.jsx
@@ -113,20 +113,24 @@ export default function christmas2023() {
                         </tbody>
                     </table>
                 </div>
-
+                <div className='pb-12 pt-6'>
+                    <Image src={'/poster-natal.png'}
+                        width={800}
+                        height={800} />
+                </div>
                 <div className='px-6 py-8 text-gray-900' id="TimothyOng">
                     <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 dark:text-white">The Legend of Christmas</h1>
 
                     <div class="grid grid-cols-1 md:gap-6 md:grid-cols-2">
-                        <p class="mb-8 text-lg font-normal"> 
-                        “Incoming!”, Santa screamed as a wall of fire surrounded the sleigh. <br />
-                        “Man the cannons!” I stumbled across the sleigh deck to a canon,
-                        my sweaty palms drenching the rope handles. Fire the canons Bang, Bang, Bang.  We all shot at the trolls riding dragons.
-                        “They're coming aboard! Get down!”, Santa yelled as the sleigh zoomed down earth's atmosphere. </p>
+                        <p class="mb-8 text-lg font-normal">
+                            “Incoming!”, Santa screamed as a wall of fire surrounded the sleigh. <br />
+                            “Man the cannons!” I stumbled across the sleigh deck to a canon,
+                            my sweaty palms drenching the rope handles. Fire the canons Bang, Bang, Bang.  We all shot at the trolls riding dragons.
+                            “They're coming aboard! Get down!”, Santa yelled as the sleigh zoomed down earth's atmosphere. </p>
                         <blockquote class="mb-3">
-                            <Image src={'/santasleigh-burning.jpeg'} 
-                            height={200}
-                            width={200}/>
+                            <Image src={'/santasleigh-burning.jpeg'}
+                                height={400}
+                                width={400} />
                         </blockquote>
                     </div>
                     <p className='mb-8 text-lg font-normal'>
@@ -138,10 +142,16 @@ export default function christmas2023() {
 
                         They've got Santa! Prepare to bail! Boom! <br />
                     </p>
-
-                    <p className='mb-8 text-lg font-normal'>
-                        A few days later. Beep beep beep (heart monitor). White curtains, beeping machines “Where am I?” I had no idea how I got here but there I was, on a white bed in the middle of a room surrounded by people in blue uniforms. My bones felt broken, my head was bulging, and I couldn't feel my legs.  I sat up to see myself plugged into many machines a breathing mask on my face, a stretcher underneath my body and a stone hard cast on my arm.  I knew where I was. I was at the hospital, but not the normal elf hospital - the human hospital. <br />
-                    </p>
+                    <div className='grid grid-cols-1 md:gap-6 md:grid-cols-2'>
+                        <p className='mb-8 text-lg font-normal'>
+                            A few days later. Beep beep beep (heart monitor). White curtains, beeping machines “Where am I?” I had no idea how I got here but there I was, on a white bed in the middle of a room surrounded by people in blue uniforms. My bones felt broken, my head was bulging, and I couldn't feel my legs.  I sat up to see myself plugged into many machines a breathing mask on my face, a stretcher underneath my body and a stone hard cast on my arm.  I knew where I was. I was at the hospital, but not the normal elf hospital - the human hospital. <br />
+                        </p>
+                        <blockquote class="mb-3">
+                            <Image src={'/hospital-room.jpeg'}
+                                height={400}
+                                width={400} />
+                        </blockquote>
+                    </div>
 
                     <p className='mb-8 text-lg font-normal'>
                         “Where's Santa?!!!” I yelled. Every person in the room looked at me, then started laughing. “Santa's not real.  You must have hit your head pretty hard”, said one of them and continued chuckling.
@@ -156,8 +166,15 @@ export default function christmas2023() {
                     <p className='mb-8 text-lg font-normal'>I jolted up and dragged the machines attached to me to the neighbouring rooms and there they were both on white beds sitting up and looking around. Chuck, Maisie, I ran to them both. “How did we get here?”, they simultaneously said. Which made me realise I don't know how we got here.
                     </p>
 
-                    <p className='mb-8 text-lg font-normal'>Just then a man in a white coat burst in. “Hello”, he said.  “My name is Doctor Edward and I'll be asking you a few questions”.  Helplessly we agreed to answer them. “What do you remember from the crash?” The crash I thought that's what happened. I told the doctor everything. I told him how I was Santa's elf first in command and we were taking the sleigh on a test drive for Christmas. It was all well until we saw something in the distance, a large ship. Painted black with skulls all over it and it had a strange purple glow. Before we knew it the ship had released some sort of purple gas blocking our field of view. We could see nothing at all. Then it happened trolls riding dragons shot walls of fire at us burning our engines soon enough they all started boarding our sleigh they took out all our reindeers and the sleigh started to fall.  Then they took Santa and left us here to crash.
-                    </p>
+                    <div className='grid grid-cols-1 md:gap-6 md:grid-cols-2'>
+                        <blockquote class="mb-3">
+                            <Image src={'/doctor-hospital-talking.jpg'}
+                                height={400}
+                                width={400} />
+                        </blockquote>
+                        <p className='mb-8 text-lg font-normal'>Just then a man in a white coat burst in. “Hello”, he said.  “My name is Doctor Edward and I'll be asking you a few questions”.  Helplessly we agreed to answer them. “What do you remember from the crash?” The crash I thought that's what happened. I told the doctor everything. I told him how I was Santa's elf first in command and we were taking the sleigh on a test drive for Christmas. It was all well until we saw something in the distance, a large ship. Painted black with skulls all over it and it had a strange purple glow. Before we knew it the ship had released some sort of purple gas blocking our field of view. We could see nothing at all. Then it happened trolls riding dragons shot walls of fire at us burning our engines soon enough they all started boarding our sleigh they took out all our reindeers and the sleigh started to fall.  Then they took Santa and left us here to crash.
+                        </p>
+                    </div>
 
                     <p className='mb-8 text-lg font-normal'>
                         “Yeah, let's put him for x-ray he's gone crazy”, Doctor Edward said. “No! I have not gone crazy we're leaving!” I grabbed Chuck and Maisie and stomped out of the room.
@@ -168,10 +185,16 @@ export default function christmas2023() {
                     <p className='mb-8 text-lg font-normal'>
                         Outside the hospital was a building surrounded by a strange purple glow the same as we saw on the ship.  We knew it had to have a link to the ship and we needed to find Santa. “You ready?” I said to the others.  “We were born ready!” they replied.  We walked over to the building and noticed there was no door something wasn't right. There was no obvious way in. So there must be another way, a secret way possibly.
                     </p>
-
-                    <p className='mb-8 text-lg font-normal'>
-                        We got on our hands and knees and crawled around the building looking for an open window or a trapdoor when we found something a manhole. The cover was loose, and it was right next to the building - this had to be the entrance. We stripped of the manhole cover and jumped in to find a massive room with skulls all over it. We knew we were in the building.  We slowly walk around and spot a staircase.  We sprint up it to see Santa behind bars surrounded by a wall of trolls and dragons. We need to save him I told the others we need a plan.
-                    </p>
+                    <div className='grid grid-cols-1 md:gap-6 md:grid-cols-2'>
+                        <blockquote class="mb-3">
+                            <Image src={'/2boys-1gril-crawling.jpg'}
+                                height={400}
+                                width={400} />
+                        </blockquote>
+                        <p className='mb-8 text-lg font-normal'>
+                            We got on our hands and knees and crawled around the building looking for an open window or a trapdoor when we found something a manhole. The cover was loose, and it was right next to the building - this had to be the entrance. We stripped of the manhole cover and jumped in to find a massive room with skulls all over it. We knew we were in the building.  We slowly walk around and spot a staircase.  We sprint up it to see Santa behind bars surrounded by a wall of trolls and dragons. We need to save him I told the others we need a plan.
+                        </p>
+                    </div>
 
                     <p className='mb-8 text-lg font-normal'>
                         Just then a large man walked in surrounded with the purple glow we saw before. “Well, well, well”.  He said, “If it isn't Santa Clause”.
@@ -213,6 +236,9 @@ export default function christmas2023() {
                     <p className='mb-8 text-lg font-normal'>
                         3 2 1.  We all jump out and slash our weapons through the trolls within 3 seconds.  It's chaos.  An alarm goes off.  Dragons and trolls rush the room all coming for us.  But being short has its advantages we all jump and none of them manage to grab us.  They end up tangling themselves in a pile jump.  We all jump onto a bar above us as more dragons flood the room and start scratching their own trolls.  In all the chaos, we launch Maisie to the cell of Santa above our heads a clip him free.
                     </p>
+                    <Image src="/dragon-and-trolls.jpg"
+                        width={400}
+                        height={400} />
 
                     <p className='mb-8 text-lg font-normal'>
                         Chuck and Maisie both take a jab at shadow Santa but he uses some sort of force to push it back into them. “You silly elves!” He bellows,” Your no match for my shadow essence”.  Shadow essence was illegal every where in the world it caused people to destroy and kill. We had to find the source.  I noticed the bright beam was coming from a little tube tied around his neck.  I needed to destroy it.  I sprinted around him taking occasional jumps and slides to wear him out until finally I jump up and take a jab straight through his chest.
@@ -240,6 +266,9 @@ export default function christmas2023() {
                     <p className='mb-8 text-lg font-normal'>
                         “GO!” Santa yelled as the reindeer's legs started to flutter with shiny dust. “Umm, what is that?” One of the elves squeaked nervously.
                     </p>
+                    <Image className='py-4' src='/santa-on-sleigh.jpg'
+                        height={400}
+                        width={400} />
 
                     <p className='mb-8 text-lg font-normal'>
                         “I'm sure, it's just a-AAAAAHHHH!” Santa screamed.  The sleigh crashed into a huge iceberg and sunk down as Santa, the elves and the reindeers all drowned in the cold, icy water.
@@ -264,7 +293,10 @@ export default function christmas2023() {
                     <p className='mb-8 text-lg font-normal'>
                         “It's time to steal Christmas! PROPERLY!”
                     </p>
-
+                    <Image src='/Evil-elf.jpg'
+                        className='py-4'
+                        width={400}
+                        height={400} />
                     <p className='mb-8 text-lg font-normal'>
                         The Grinch boomed as he held something high in his furry, green hands.  “With my newly found, Multidimensional watch, I'll be able to steal Christmas in other dimensions!”
                     </p>
@@ -273,10 +305,15 @@ export default function christmas2023() {
                         Stumpy was in shock, he had seen other inventions by the Grinch, but those were nothing compared to this.  This could permanently end Christmas forever! Stumpy had been sneakily hiding in the Grinch's lair for 15 years straight, by stealing food and water, but it was time for him to do really do something.
                     </p>
 
+
                     <p className='mb-8 text-lg font-normal'>
                         As soon as the Grinch pressed the button for dimensional travel, Stumpy leaped in and stole the watch.
                     </p>
 
+                    <Image src='/green-elf-dive.jpg'
+                        className='py-4'
+                        width={400}
+                        height={400} />
                     <p className='mb-8 text-lg font-normal'>
                         “HEY! COME BACK HERE YOU ELF!”
                     </p>
@@ -325,7 +362,10 @@ export default function christmas2023() {
                     <h3 className="mb-4 text-lg font-extrabold tracking-tight leading-none text-gray-900">
                         BACK AT THE LAIR
                     </h3>
-
+                    <Image src='/green-elf-angry.jpg'
+                        className='py-4'
+                        width={400}
+                        height={400} />
                     <p className='mb-8 text-lg font-normal'>
                         “OH, COME ON!” The Grinch yelled angrily.
                     </p>
@@ -357,7 +397,10 @@ export default function christmas2023() {
                     <p className='mb-8 text-lg font-normal'>
                         “C-O-M-E   O-N   O-U-T-!”, the robots beeped
                     </p>
-
+                    <Image src='/robot-searching.jpg'
+                        className='py-4'
+                        height={400}
+                        width={400} />
                     <p className='mb-8 text-lg font-normal'>
                         “What do I do? What do I do?”  Stumpy thought.  “Wait a minute, that's right! If I get the Grinch's watch, I can control all the robots!”.
                     </p>
@@ -413,7 +456,10 @@ export default function christmas2023() {
                     <p className='mb-8 text-lg font-normal'>
                         “So? Can you stop being bad?” Stumpy asked.
                     </p>
-
+                    <Image src='/grinch-eat-cake.jpg'
+                        className='py-4'
+                        height={400}
+                        width={400} />
                     <p className='mb-8 text-lg font-normal'>
                         “I guess so”, the Grinch said half-heartedly.
                     </p>
